ee0af50fcfe6f72d42ddfdc9fcfe649515aaaaf5
reza gholami <reza.gholami.m.1998@gmail.com>
2024-02-25 14:34:07 +0330

    Add : create Routes in component App for /RezaGholami Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-// import './App.css'
-
+import "./App.css";
+import CvTemplate from "./pages/CvTemplate";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+////////////////////////////////////////////////
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/RezaGholami" />}></Route>
+        <Route path="/RezaGholami" element={<CvTemplate />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
