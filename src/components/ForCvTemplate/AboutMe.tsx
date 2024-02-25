8897c002ddb48e2cc1c6281669a3ab71b64d7ee3
reza gholami <reza.gholami.m.1998@gmail.com>
2024-02-25 14:30:16 +0330

    Add : create component AbouteMe.tsx

---
+++ src/components/ForCvTemplate/AboutMe.tsx
@@ -0,0 +1,149 @@
+import Boy2 from "@/assets/images/Boy2.png";
+import Dots from "@/assets/images/Dots.svg";
+import Dots2 from "@/assets/images/Dots2.svg";
+import { Typography, Box, Grid } from "@mui/material";
+const AboutMe = () => {
+  return (
+    <>
+      <Box
+        id="aboutMe"
+        sx={{ display: "flex", alignItems: "center", gap: "16px" }}
+      >
+        <Typography
+          variant="h5"
+          component="h3"
+          sx={{
+            fontSize: "1.5rem",
+            fontWeight: "500",
+            color: "#fff",
+            textWrap: "nowrap",
+          }}
+        >
+          <Typography
+            component="span"
+            sx={{
+              height: "28px",
+              color: "#C778DD",
+              fontSize: "1.5rem",
+              fontWeight: "bold",
+            }}
+          >
+            #&nbsp;
+          </Typography>
+          درباره من
+        </Typography>
+        <Box
+          sx={{
+            width: { xs: "100%", sm: "40%" },
+            height: "1px",
+            background: "#C778DD",
+          }}
+        ></Box>
+      </Box>
+      <Grid
+        container
+        sx={{
+          p: { xs: "50px 0", md: " 0 0 50px" },
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          position: "relative",
+          flexDirection: { xs: "column-reverse", md: "row" },
+          gap: { xs: "30px", md: "0" },
+        }}
+      >
+        <Grid
+          item
+          xs={12}
+          md={7}
+          sx={{
+            display: "flex",
+          }}
+        >
+          <Typography
+            sx={{
+              lineHeight: "26px",
+              color: "#ABB2BF",
+              textAlign: { xs: "center", sm: "justify" },
+            }}
+          >
+            سلام ، من رضا هستم !
+            <br />
+            <br />
+            فارغ التحصیل رشته کامپیوتر با معدل الف از دانشگاه دولتی تربت حیدریه
+            <br />
+            <br />
+            علاقه‌مند به ساخت رابط‌های کاربری زیبا، واکنش‌پذیر و کارآمد برای
+            وبسایت‌ها و اپلیکیشن‌های مختلف هستم.
+            <br />
+            <br />
+            همواره به دنبال یادگیری و بهبود مهارت‌هایم در زمینه برنامه‌نویسی وب
+            هستم، تا بتوانم بهترین نسخه خودم را به عنوان یک برنامه‌نویس
+            فرانت‌اند ارائه دهم.
+            <br />
+            <br />
+            من یک برنامه‌نویس فرانت اند با اشتیاق بالا و علاقه‌مند به طراحی رابط
+            کاربری زیبا هستم. به بهبود تجربه کاربری وب و ایجاد وبسایت‌های
+            قدرتمند و جذاب علاقه فراوان دارم.
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={5}
+          sx={{
+            display: "flex",
+            justifyContent: { xs: "center", md: "flex-end" },
+            width: { xs: "70%", md: "auto" },
+          }}
+        >
+          <Box sx={{ position: "relative", width: { xs: "100%", sm: "auto" } }}>
+            <Box
+              component="img"
+              src={Boy2}
+              sx={{
+                transform: "scale(-1,1)",
+                objectFit: "contain",
+                width: { xs: "100%", md: "300px" },
+                height: { xs: "300px", md: "auto" },
+              }}
+            />
+
+            <Box
+              component="img"
+              src={Dots}
+              className="slideInRightMin"
+              sx={{
+                position: "absolute",
+                left: { xs: "-10px", md: "30px" },
+                top: { xs: "30px", md: "50px" },
+                zIndex: "-1",
+              }}
+            />
+            <Box
+              component="img"
+              src={Dots2}
+              sx={{
+                position: "absolute",
+                bottom: "50px",
+                right: "30px",
+              }}
+              className="slideInUp"
+            />
+            <Box
+              sx={{
+                width: { xs: "100%", md: "100%" },
+                height: "2px",
+                background: "#C778DD",
+                transform: {
+                  xs: "translate(0px, -10px)",
+                  sm: "translate(-0px, -10px)",
+                },
+              }}
+            ></Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+export default AboutMe;
