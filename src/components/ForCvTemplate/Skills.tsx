b2b54550a1da099e1d65ca870112c6a0cecaab8f
reza gholami <reza.gholami.m.1998@gmail.com>
2024-02-25 14:30:31 +0330

    Add : create component Skills.tsx

---
+++ src/components/ForCvTemplate/Skills.tsx
@@ -0,0 +1,250 @@
+import { Typography, Box, Grid } from "@mui/material";
+import GroupShape from "@/assets/images/Group-shape.png";
+const Skills = () => {
+  return (
+    <>
+      <Box
+        id="skills"
+        sx={{ display: "flex", alignItems: "center", gap: "16px" }}
+      >
+        <Typography
+          variant="h5"
+          component="h3"
+          sx={{
+            color: "#fff",
+            fontWeight: "500",
+            textWrap: "nowrap",
+            fontSize: "1.5rem",
+          }}
+        >
+          <Typography
+            component="span"
+            sx={{
+              height: "28px",
+              color: "#C778DD",
+              fontSize: "1.5rem",
+              fontWeight: "bold",
+            }}
+          >
+            #&nbsp;
+          </Typography>
+          مهارت ها
+        </Typography>
+        <Box
+          sx={{
+            width: { xs: "100%", sm: "70%" },
+            height: "1px",
+            background: "#C778DD",
+          }}
+        ></Box>
+      </Box>
+      <Grid
+        container
+        sx={{
+          p: "50px 0",
+          alignItems: "center",
+          position: "relative",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <Grid item xs={12} md={8} container sx={{ gap: "16px" }}>
+          <Grid
+            item
+            xs={12}
+            md={3}
+            sx={{ display: "flex", flexDirection: "column", gap: "16px" }}
+          >
+            <Box sx={{ border: "1px solid #ABB2BF" }}>
+              <Typography
+                variant="h6"
+                sx={{
+                  color: "#fff",
+                  padding: "8px",
+                  fontWeight: "600",
+                  lineHeight: "21px",
+                  borderBottom: "1px solid #ABB2BF",
+                }}
+              >
+                زبان
+              </Typography>
+              <Typography
+                sx={{
+                  padding: "8px",
+                  color: "#ABB2BF",
+                  textAlign: "left",
+                  lineHeight: "21px",
+                }}
+              >
+                JavaScript
+              </Typography>
+              <Typography
+                sx={{
+                  padding: "8px",
+                  color: "#ABB2BF",
+                  textAlign: "left",
+                  lineHeight: "21px",
+                }}
+              >
+                TypeScript
+              </Typography>
+            </Box>
+            <Box sx={{ border: "1px solid #ABB2BF", textAlign: "left" }}>
+              <Typography
+                variant="h6"
+                sx={{
+                  color: "#fff",
+                  padding: "8px",
+                  fontWeight: "600",
+                  textAlign: "right",
+                  lineHeight: "21px",
+                  borderBottom: "1px solid #ABB2BF",
+                }}
+              >
+                فریمورک ها
+              </Typography>
+              <Typography
+                sx={{ padding: "8px", lineHeight: "21px", color: "#ABB2BF" }}
+              >
+                React Js
+              </Typography>
+              <Typography
+                sx={{ padding: "8px", lineHeight: "21px", color: "#ABB2BF" }}
+              >
+                Mui
+              </Typography>
+              <Typography
+                sx={{ padding: "8px", lineHeight: "21px", color: "#ABB2BF" }}
+              >
+                Tailwind Css
+              </Typography>
+              <Typography
+                sx={{ padding: "8px", lineHeight: "21px", color: "#ABB2BF" }}
+              >
+                Bootstrap v4,v5
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={3}
+            sx={{ display: "flex", flexDirection: "column", gap: "16px" }}
+          >
+            <Box sx={{ border: "1px solid #ABB2BF", textAlign: "left" }}>
+              <Typography
+                variant="h6"
+                sx={{
+                  color: "#fff",
+                  padding: "8px",
+                  fontWeight: "600",
+                  textAlign: "right",
+                  lineHeight: "21px",
+                  borderBottom: "1px solid #ABB2BF",
+                }}
+              >
+                بقیه موارد
+              </Typography>
+              <Typography
+                sx={{ padding: "8px", lineHeight: "21px", color: "#ABB2BF" }}
+              >
+                Html
+              </Typography>
+              <Typography
+                sx={{ padding: "8px", lineHeight: "21px", color: "#ABB2BF" }}
+              >
+                Css
+              </Typography>
+
+              <Typography
+                sx={{ padding: "8px", lineHeight: "21px", color: "#ABB2BF" }}
+              >
+                Flex & Grid
+              </Typography>
+
+              <Typography
+                sx={{ padding: "8px", lineHeight: "21px", color: "#ABB2BF" }}
+              >
+                Responsive Design
+              </Typography>
+            </Box>
+            <Box sx={{ border: "1px solid #ABB2BF", textAlign: "left" }}>
+              <Typography
+                variant="h6"
+                sx={{
+                  color: "#fff",
+                  padding: "8px",
+                  fontWeight: "600",
+                  lineHeight: "21px",
+                  textAlign: "right",
+                  borderBottom: "1px solid #ABB2BF",
+                }}
+              >
+                پیش پردازنده
+              </Typography>
+              <Typography
+                sx={{ padding: "8px", lineHeight: "21px", color: "#ABB2BF" }}
+              >
+                Sass
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <Box sx={{ border: "1px solid #ABB2BF" }}>
+              <Typography
+                variant="h6"
+                sx={{
+                  color: "#fff",
+                  padding: "8px",
+                  fontWeight: "600",
+                  lineHeight: "21px",
+                  borderBottom: "1px solid #ABB2BF",
+                }}
+              >
+                ابزارها
+              </Typography>
+              <Typography
+                sx={{
+                  padding: "8px",
+                  color: "#ABB2BF",
+                  textAlign: "left",
+                  lineHeight: "21px",
+                }}
+              >
+                Git
+              </Typography>
+              <Typography
+                sx={{
+                  padding: "8px",
+                  color: "#ABB2BF",
+                  textAlign: "left",
+                  lineHeight: "21px",
+                }}
+              >
+                Figma
+              </Typography>
+              <Typography
+                sx={{
+                  padding: "8px",
+                  color: "#ABB2BF",
+                  textAlign: "left",
+                  lineHeight: "21px",
+                }}
+              >
+                VsCode
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          md={4}
+          sx={{ display: { xs: "none", md: "block", textAlign: "left" } }}
+        >
+          <Box component="img" src={GroupShape} />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default Skills;
