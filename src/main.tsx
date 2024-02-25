a6909caedcf392531080d4ad89d223976edfbd19
reza gholami <reza.gholami.m.1998@gmail.com>
2024-02-25 14:33:34 +0330

    Add : font in ThemeProvider in component main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import React from 'react'
+import App from './App.tsx'
+import ReactDOM from 'react-dom/client'
+import { createTheme, Theme, ThemeProvider } from "@mui/material/styles";
+/////////////////////////////////////////////////////////////////////
+const theme = createTheme({
+  typography: {
+    fontFamily:
+      'YekanBakh',
+    }
+  })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
