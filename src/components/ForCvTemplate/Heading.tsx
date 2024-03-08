5a15548e1b78d4e0afd2990cdbedb8389a307d2d
reza gholami <reza.gholami.m.1998@gmail.com>
2024-03-09 00:40:49 +0330

    Add : create component reuseable Heading.tsx

---
+++ src/components/ForCvTemplate/Heading.tsx
@@ -0,0 +1,38 @@
+import { Box, Typography } from "@mui/material";
+const Heading = ({ title, borderWidth = "0",id }) => {
+  return (
+    <Box id={id} sx={{ display: "flex", alignItems: "center", gap: "16px" }}>
+      <Typography
+        variant="h5"
+        component="h3"
+        sx={{
+          color: "#fff",
+          fontWeight: "500",
+          textWrap: "nowrap",
+          fontSize: "1.5rem",
+        }}
+      >
+        <Typography
+          component="span"
+          sx={{
+            height: "28px",
+            color: "#C778DD",
+            fontSize: "1.5rem",
+            fontWeight: "bold",
+          }}
+        >
+          #&nbsp;
+        </Typography>
+        {title}
+      </Typography>
+      <Box
+        sx={{
+          width: { xs: "100%", sm: `${borderWidth}` },
+          height: "1px",
+          background: "#C778DD",
+        }}
+      ></Box>
+    </Box>
+  );
+};
+export default Heading;
